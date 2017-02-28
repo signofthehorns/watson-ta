3a71256b1815922c0262c5dde9934d0e2ea84213
flanagan.166@osu.edu <dltn@users.noreply.github.com>
2017-02-28 16:09:03 -0500

    s3 is down - benign commit to trigger CI build

--- test/test_alchemy.py
+++ test/test_alchemy.py
@@ -2,4 +2,4 @@ def test_discovery_test():
 	from www.api.views import alchemy
 
 	# (This isn't a meaningful alchemy request, just testing the service for now)
-	alchemy.GetAlchemyRequest("hello", "world")
+	alchemy.GetAlchemyRequest("hello", "world!")
