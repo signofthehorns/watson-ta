c90af61dbf984d99d348155ee1ee3d445120cedd
Tyler Zeller <tylermzeller@gmail.com>
2017-03-05 15:18:51 -0500

    Add ability to pull specific chapters from textbooks

--- textbook-tool/tool.py
+++ textbook-tool/tool.py
@@ -30,7 +30,6 @@ def convert_pdf_to_txt(fp, pagenos=set(), maxpages=0):
     device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     password = ""
-    maxpages = 0
     caching = True
     for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
         interpreter.process_page(page)
@@ -39,28 +38,49 @@ def convert_pdf_to_txt(fp, pagenos=set(), maxpages=0):
     retstr.close()
     return text
 
-def get_table_of_contents(fp):
+def get_top_level_sections(fp):
     parser = PDFParser(fp)
     document = PDFDocument(parser, '')
     pages = dict( (page.pageid, pageno) for (pageno,page)
                   in enumerate(PDFPage.create_pages(document)) )
 
+
+    section_pages = list()
     # Get the outlines of the document.
     try:
         outlines = document.get_outlines()
         for (level,title,dest,a,se) in outlines:
+            if level != 1: continue
             pageno = None
             if dest:
                 dest = resolve_dest(dest)
                 pageno = pages[dest[0].objid]
-            print "{l} - {t} {p}".format(l=level, t=title.encode('utf-8'), p=pageno)
+                section_pages.append((title, pageno))
+            #print "{l} - {t} {p}".format(l=level, t=title.encode('utf-8'), p=pageno)
     except PDFNoOutlines:
         pass
     parser.close()
+    return section_pages
 
-def extract_chapters(fp, chapters='all'):
+def convert_top_levels_to_pagenos(fp, top_levels):
+    top_level_pages = get_top_level_sections(fp)
+    pagenos = []
+    #TODO(tylermzeller) support all top levels extraction
+    if top_levels == []:
+        pass
+    for level in top_levels:
+        start_idx = level
+        end_idx = level + 1
+        pagenos.append(range(top_level_pages[start_idx][1], top_level_pages[end_idx][1]))
+    return [page for l in pagenos for page in l]
 
+
+def extract_top_levels(fp, top_levels=[]):
+    pagenos = convert_top_levels_to_pagenos(fp, top_levels)
+
+    return convert_pdf_to_txt(fp, pagenos=set(pagenos))
 
 #print convert_pdf_to_txt(fp)
-get_table_of_contents(fp)
+txt = extract_top_levels(fp, top_levels=[1,2])
+print txt
 fp.close()
