3c002f4bb7cc21f255e12ab9c8cf730021dee243
David Soller <soller.23@osu.edu>
2017-03-23 18:06:59 -0400

    Added DC environment config

--- textbook-tool/doc-conv.py
+++ textbook-tool/doc-conv.py
@@ -1,19 +1,20 @@
+import os
 from watson_developer_cloud import DocumentConversionV1
 
 document_conversion = DocumentConversionV1(
-    username='',
-    password='',
+    username=os.environ['watson_dc_username'],
+    password=os.environ['watson_dc_password'],
     version='2017-03-23')
 
 try:
-  # Example of retrieving html or plain text
-  with open('chapter1.pdf', 'r') as document:
-      with open('chapter1-doc-conv.txt', 'w') as output_stream:
-          config = {'conversion_target': DocumentConversionV1.NORMALIZED_TEXT}
-          result = document_conversion.convert_document(
-              document=document, config=config).content
-          output_stream.write(result)
-    #   print(document_conversion.convert_document(
-    #       document=document, config=config).content)
+    # Example of retrieving html or plain text
+    with open('chapter1.pdf', 'r') as document:
+        with open('chapter1-doc-conv.txt', 'w') as output_stream:
+            config = {'conversion_target': DocumentConversionV1.NORMALIZED_TEXT}
+            result = document_conversion.convert_document(
+                document=document, config=config).content
+            output_stream.write(result)
+      #   print(document_conversion.convert_document(
+      #       document=document, config=config).content)
 except WatsonException as e:
-  print e
+    print e
