664fb2161b3e9d1e07c9edc399ba0e96b6b2d161
Dalton Flanagan <dltn@users.noreply.github.com>
2017-03-23 18:27:13 -0400

    hotfix for homepage redirect
    missing regex close redirects api/ calls to homepage

--- www/pages/urls.py
+++ www/pages/urls.py
@@ -4,5 +4,5 @@ from views import GetHomePage, GetTeamPage
 urlpatterns = [
     url(r'^team/$', GetTeamPage, name='The Team'),
     url(r'^home/$', GetHomePage, name='The Homepage'),
-    url(r'^', GetHomePage, name='The Homepage')
+    url(r'^$', GetHomePage, name='The Homepage')
 ]
