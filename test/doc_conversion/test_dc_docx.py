e1eefc3861250ca46f510d71215e6a2558eb1cf2
dltn <d@lton.name>
2017-04-11 17:18:21 -0400

    fix word doc body

--- test/doc_conversion/test_dc_docx.py
+++ test/doc_conversion/test_dc_docx.py
@@ -2,5 +2,5 @@ import www.api.views.doc_conversion
 
 def test_alchemy_example():
     test_file = "test/doc_conversion/test.docx"
-    expected_text = 'Microsoft Word - This is a test Word document.docx\n\nThis is a test PDF document!'
+    expected_text = 'Microsoft Word - This is a test Word document.docx\n\nThis is a test Word document!'
     www.api.views.doc_conversion.get_doc_coversion(open(test_file))
