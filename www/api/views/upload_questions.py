e77498f9652fb76ecc14ee5f12fcceb55dfd08fd
David Soller <soller.23@osu.edu>
2017-04-15 16:47:20 -0400

    Partial MC choice extraction
    "Works" if the question's options are the following question prompt
    after doc_conversion (ex: 3-questions.pdf). BUT fails if the options are
    within a single line...
    
    For testing one will need to reclassify the MC question on
    3-questions.pdf from TF to MC to see the change

--- www/api/views/upload_questions.py
+++ www/api/views/upload_questions.py
@@ -3,7 +3,6 @@ import re
 from django import forms
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from classified import Classified
 from doc_conversion import get_doc_coversion
 from nlc_classify import get_question_type
 
@@ -13,27 +12,40 @@ def upload_questions_file(request):
     if request.method == 'POST':
         if forms.Form(request.POST, request.FILES).is_valid():
             questions_raw = get_doc_coversion(request.FILES['file'])
-            questions_raw = questions_raw.split('\n')
+            questions_raw = filter(
+                (lambda question: question.strip()), questions_raw.split('\n'))
+            print ""
+            print ">> Raw Question: ", questions_raw
+            print ""
             questions = []
             i = 0
             while i < len(questions_raw):
-                question = questions_raw[i].strip()
-                nextIsAlpha = True
-                while nextIsAlpha and (i < len(questions_raw)-1):
-                    if questions_raw[i+1].strip() == '':
-                        i = i + 1
-                    elif questions_raw[i+1].strip()[0].isalpha():
-                        question = question + " " + questions_raw[i+1].strip()
-                        i = i + 1
-                    else:
-						nextIsAlpha = False
-                if question != '' and question != 'no title':
+                question = {
+                    "prompt":  questions_raw[i],
+                    "choices": [],
+                }
+
+                # Why isalpha works somewhat?
+                #  Our regular question is: 1. What is...
+                #  For a multiple choice line it will look like: a. cake b. pie
+                # So, If there is not a number we're assuming it's a multiple
+                # choice question
+                while (i < len(questions_raw) - 1) and questions_raw[i + 1][0].isalpha():
+                    print ">> Parsing Choices"
+                    choices = re.split(
+                        '(?P<choice>(?:.(?![abcd][\.]))*)', questions_raw[i + 1])
+                    question["choices"] = filter(
+                        (lambda choice: choice.strip()), choices)
+                    i = i + 1
+                if question["prompt"] != 'no title':
                     questions.append({
-                        'type': get_question_type(question),
-                        'prompt': question,
+                        'type': get_question_type(question["prompt"]),
+                        'prompt': question["prompt"],
+                        'choices': question["choices"],
                     })
-                    # TODO: Parse out choices for MC
                 i = i + 1
+
+            print questions
             return JsonResponse({
                 'success': True,
                 'questions': questions
