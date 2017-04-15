5c27beebc8a8a365fcbb8aee8bbd1e308ecbd697
David Soller <soller.23@osu.edu>
2017-04-15 19:17:07 -0400

    Fixes the majority of Multiple Choice parsing issue
    - Questions choices that are appended to the prompt
    - Questions choices that are held in the following questions

--- www/api/views/upload_questions.py
+++ www/api/views/upload_questions.py
@@ -6,6 +6,13 @@ from django.views.decorators.csrf import csrf_exempt
 from doc_conversion import get_doc_coversion
 from nlc_classify import get_question_type
 
+"""
+Takes text and returns the extracted choices
+"""
+def extract_choices(text):
+    choices = re.split('(?P<choice>(?:.(?![abcd][\.]))*)', text)
+    choices = filter((lambda choice: choice.strip()), choices)
+    return choices
 
 @csrf_exempt
 def upload_questions_file(request):
@@ -14,34 +21,49 @@ def upload_questions_file(request):
             questions_raw = get_doc_coversion(request.FILES['file'])
             questions_raw = filter(
                 (lambda question: question.strip()), questions_raw.split('\n'))
-            print ""
-            print ">> Raw Question: ", questions_raw
-            print ""
+            print ''
+            print '>> Raw Question: ', questions_raw
+            print ''
             questions = []
-            i = 0
+            i = 1
             while i < len(questions_raw):
-                question = {
-                    "prompt":  questions_raw[i],
-                    "choices": [],
-                }
+                prompt = questions_raw[i]
 
-                # Why isalpha works somewhat?
-                #  Our regular question is: 1. What is...
-                #  For a multiple choice line it will look like: a. cake b. pie
-                # So, If there is not a number we're assuming it's a multiple
-                # choice question
-                while (i < len(questions_raw) - 1) and questions_raw[i + 1][0].isalpha():
-                    print ">> Parsing Choices"
-                    choices = re.split(
-                        '(?P<choice>(?:.(?![abcd][\.]))*)', questions_raw[i + 1])
-                    question["choices"] = filter(
-                        (lambda choice: choice.strip()), choices)
-                    i = i + 1
-                if question["prompt"] != 'no title':
+                # Dropping:
+                # - Title (i = 1 drops)
+                # - Any lines that don't start numbered
+                if prompt[0].isalnum():
+                    question_type = get_question_type(prompt)
+                    question_choices = []
+
+                    if question_type['class_name'] == 'MC':
+                        choices = extract_choices(prompt)
+                        prompt = choices[0]
+                        question_choices = choices[1:]
+
+                    # Why isalpha works somewhat?
+                    #  Our regular question is: 1. What is...
+                    #  For a multiple choice line it will look like: a. cake b. pie
+                    # So, If there is not a number we're assuming it's a multiple
+                    # choice question
+                    while (i < len(questions_raw) - 1) and questions_raw[i + 1][0].isalpha():
+                        print ">> Parsing Choices"
+                        question_choices = question_choices + extract_choices(questions_raw[i + 1])
+                        question_type = {
+                            'class_name': 'MC',
+                            'confidence': 1
+                        }
+                        i = i + 1
+
+                    # Is question_choices empty?
+                    if not question_choices:
+                        question_choices = ['A', 'B', 'C', 'D']
+
+                    # Create final question
                     questions.append({
-                        'type': get_question_type(question["prompt"]),
-                        'prompt': question["prompt"],
-                        'choices': question["choices"],
+                        'type': question_type,
+                        'prompt': prompt,
+                        'choices': question_choices,
                     })
                 i = i + 1
 
